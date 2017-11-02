2878d7a4d231297ce31dfd35790d6b5f64d738a8
Alan David Richards <alrichards@incomm.com>
2017-11-02 10:05:20 -0400

    Created all components that I should need. Working on getting the different models set up.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Still test recipe', 'https://upload.wikimedia.org/wikipedia/commons/d/d1/Pepperoni_pizza.jpg')
+  ];
 
   constructor() { }
 
